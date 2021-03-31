06dc2ba6fb4252af57fd6dbc465ba46fd998a0e7
bthnylcn <batuhannyalcinn@gmail.com>
2021-03-31 22:53:08 +0300

    angular #1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //indexte tanıması için
+  templateUrl: './app.component.html', //datasını yöneticeği yer
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'Batu';
 }
